e29897a40803f98f692406579781a7dc1fa39738
Michael Redig <mredig@gmail.com>
2019-07-03 16:21:49 -0500

    extracted array extension for swiftdocitems to its own file

--- SwiftyDocs/Model Controllers/SortedDocController.swift
+++ SwiftyDocs/Model Controllers/SortedDocController.swift
@@ -154,32 +154,3 @@ class SwiftDocItemController {
 		return output
 	}
 }
-
-
-extension Array where Element == SwiftDocItem {
-	func enumeratedChildren() -> [SwiftDocItem] {
-		var enumerated = [SwiftDocItem]()
-		for item in self {
-			enumerated.append(item)
-			guard let itemChildren = getChildren(from: item) else { continue }
-			enumerated.append(contentsOf: itemChildren)
-		}
-
-		return enumerated
-	}
-
-	private func getChildren(from item: SwiftDocItem) -> [SwiftDocItem]? {
-		guard let children = item.properties else {
-			return nil
-		}
-		var newChilds = [SwiftDocItem]()
-		for child in children {
-			newChilds.append(child)
-			guard let unwrappedNewChildren = getChildren(from: child) else {
-				continue
-			}
-			newChilds += unwrappedNewChildren
-		}
-		return newChilds
-	}
-}
